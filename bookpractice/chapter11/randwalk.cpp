44e0c5e82565dee006e220fe6635b3d7b0e2b7a0
macos <lilong.199607@gmail.com>
2022-11-07 01:58:48 +0800

    chapter11 bookcase5

---
+++ bookpractice/chapter11/randwalk.cpp
@@ -0,0 +1,47 @@
+// using the Vector class
+// compile with the vector.cpp file
+#include <iostream>
+#include <cstdlib> // rand(), srand() prototypes
+#include <ctime> // time() time() prototype
+#include "vector.h"
+int main()
+{
+    using namespace std;
+    using VECTOR::Vector;
+    srand(time(0)); // seed random-number generator
+    double direction;
+    Vector step;
+    Vector result(0.0, 0.0);
+    unsigned long steps = 0;
+    double target;
+    double dstep;
+    cout << "Enter target distance (q to quit): ";
+    while (cin >> target)
+    {
+        cout << "Enter step length: ";
+        if (!(cin >> dstep))
+            break;
+        while (result.magval() < target)
+        {
+            direction = rand() % 360;
+            step.reset(dstep, direction, Vector::POL);
+            result = result + step;
+            steps++;
+        }
+        cout << "After " << steps << " steps, the subject "
+             << "has the following location:\n";
+        cout << result << endl;
+        result.polar_mode();
+        cout << " or\n" << result << endl;
+        cout << "Average oubward distance per step = "
+             << result.magval() / steps << endl;
+        steps = 0;
+        result.reset(0.0, 0.0);
+        cout << "Enter target distance (q to quit): ";
+    }
+    cout << "Bye!\n";
+    cin.clear();
+    while (cin.get() != '\n')
+        continue;
+    return 0;
+}
