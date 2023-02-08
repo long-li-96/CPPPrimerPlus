e58451068e948a93297b9badf2959988d9db699b
macos <lilong.199607@gmail.com>
2023-02-08 12:28:32 +0800

    test

--- test/test_order.cpp
+++ test/test_order.cpp
@@ -3,16 +3,28 @@ using std::cout;
 using std::endl;
 int main()
 {
-    int a[] = {1,2,3,4,5};
-    int i = 3;
-    cout << a[i] << endl;
-    a[i++] = 10;
-    cout << i << endl;
-    for (int j = 0; j < 5; j ++)
-        cout << a[j];
-    cout << endl;
-    cout << a[i] << endl;
-    int b = a[--i];
-    cout << b << endl;
-    return 0;
+    double a = 0.1;
+    double b = 0.2;
+    double c = 0.3;
+    cout << "a + b = " << a + b <<endl;
+    cout << "c = " << c << endl;
+    if (a+b == c)
+        cout << "a + b = c";
+    cout << "a + b != c";
+
+
+
+
+    // int a[] = {1,2,3,4,5};
+    // int i = 3;
+    // cout << a[i] << endl;
+    // a[i++] = 10;
+    // cout << i << endl;
+    // for (int j = 0; j < 5; j ++)
+    //     cout << a[j];
+    // cout << endl;
+    // cout << a[i] << endl;
+    // int b = a[--i];
+    // cout << b << endl;
+    // return 0;
 }
