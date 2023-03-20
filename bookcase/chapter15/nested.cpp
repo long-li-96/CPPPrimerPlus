d0c5aa79df8c41ec5c7c841bee8802f91a6076a1
long-li-96 <lilong.199607@gmail.com>
2023-03-20 12:46:37 +0800

    chapter15 bookcase

---
+++ bookcase/chapter15/nested.cpp
@@ -0,0 +1,34 @@
+// using a queue that has a nested class
+#include <iostream>
+
+#include <string>
+#include "queuetp.h"
+
+int main()
+{
+    using std::string;
+    using std::cout;
+    using std::cin;
+
+    QueueTp<string> cs(5);
+
+    string temp;
+
+    while (!cs.isfull())
+    {
+        cout << "Please enter your nam, you will be served in the order"
+                " of arrival.\n" 
+                "name: ";
+        getline(cin,temp);
+        cs.enqueue(temp);
+    }
+
+    cout << "THe queue is full, Processing begins!\n";
+    while (! cs.isempty())
+    {
+        cs.dequeue(temp);
+        cout << "Now processing " << temp << "...\n";
+
+    }
+    return 0;
+}
