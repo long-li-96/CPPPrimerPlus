cfb30516c7ce8409b0e0c27ec3b7b649ebe9fd8c
long-li-96 <lilong.199607@gmail.com>
2023-04-26 13:36:01 +0800

    chapter16 review

--- bookcase/chapter16/vect3.cpp
+++ bookcase/chapter16/vect3.cpp
@@ -6,6 +6,8 @@
 #include <string>
 #include <vector>
 
+// 非成员函数swap()可以交换两个类型不同的容器的内容
+
 struct Review {
   std::string title;
   int rating;
@@ -32,18 +34,24 @@ int main() {
          << " ratings: \n"
          << "Rating\tBook\n";
     // 容器指定区间的元素:[begin,end)遍历去作为ShowReview的实参
-    for_each(books.begin(), books.end(), ShowReview);
+    // 不可修改容器的内容
+    for_each(books.begin(), books.end(), ShowReview); // 等价于：for(auto x: books) ShowReview(x); // 指定引用参数，可以修改容器内容
 
+    // sort()要求容器类可以随机访问
+    // 第一个版本的sort()，使用容器元素的<运算符，容器元素是用户自定义对象时，这个对象需要有成员或非成员函数operator<()
     sort(books.begin(),
          books.end());  // 升序排序, 使用为这个容器类型元素定义的operator<()
 
-    cout << "Sorted by rating: \nRating\t Book\n";
-    for_each(books.begin(), books.end(), ShowReview);
-
+    cout << "Sorted by rating: \nRating\t Book\n";  
+    for_each(books.begin(), books.end(), ShowReview); // 等价于：for(auto x: books) ShowReview(x);
+    // random_shuffle(books.begin(), books.end()); // 随机排列books中所有元素,要求books这个容器类可以随机访问
+   
+    // 第二个版本的sort(),最后一个参数是函数对象
     sort(books.begin(), books.end(),
          worseThen);  // 使用自定义排序函数worseThen等于操作符<
     cout << "Sorted by rating: \nRating\tBook\n";
-    for_each(books.begin(), books.end(), ShowReview);
+    // for_each()最后一个参数是函数对象（可以是函数指针等）
+    for_each(books.begin(), books.end(), ShowReview); // begin到end-1的每个元素作为入参执行ShowReview
 
     // 使用随机设备作为随机数生成器的种子
     std::random_device rd;
@@ -60,6 +68,7 @@ int main() {
   // vector<double>::iterator pr; 声明一个迭代器
 }
 
+// 全排序
 bool operator<(const Review& r1, const Review& r2) {
   if (r1.title < r2.title)
     return true;
@@ -69,6 +78,7 @@ bool operator<(const Review& r1, const Review& r2) {
     return false;
 }
 
+// 完整弱排序
 bool worseThen(const Review& r1, const Review& r2) {
   if (r1.rating < r2.rating)
     return true;
